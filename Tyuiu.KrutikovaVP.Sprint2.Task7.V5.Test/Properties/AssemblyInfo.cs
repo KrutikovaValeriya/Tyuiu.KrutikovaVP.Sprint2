6b69c2cffd71d1bb7640d650f89b47bc4d8fb409
Tyuiu.KrutikovaVP@outlook.com <lera.krutikova.2005@mail.ru>
2023-10-15 19:52:33 +0500

    Task7

---
+++ Tyuiu.KrutikovaVP.Sprint2.Task7.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KrutikovaVP.Sprint2.Task7.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KrutikovaVP.Sprint2.Task7.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02ed18ba-cc68-4819-b671-5277fa9c39c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
